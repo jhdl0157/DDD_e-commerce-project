2bda1d252acba2e022ee99505141749b84ccba34
jhdl0157 <jhdl0157@naver.com>
2022-06-28 00:12:52 +0900

    InfraStructure 추가

---
+++ src/main/java/com/example/order/common/exception/EntityNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.order.common.exception;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
---
+++ src/main/java/com/example/order/common/exception/IllegalStatusException.java
@@ -0,0 +1,12 @@
+package com.example.order.common.exception;
+
+public class IllegalStatusException extends BaseException {
+
+    public IllegalStatusException() {
+        super(ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+
+    public IllegalStatusException(String message) {
+        super(message, ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+}
